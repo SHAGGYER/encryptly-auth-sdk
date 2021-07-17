6d21042015c5e9e968cbe1dfbad21f20cb89462b
Mikolaj Marciniak <mikoo1991@yahoo.com>
2021-07-18 00:00:58 +0200

    standardized the fields arg

--- src/methods.js
+++ src/methods.js
@@ -23,11 +23,13 @@ exports.checkToken = async ({ serverUrl, token, clientId, clientSecret }) => {
   return returnObj;
 };
 
-exports.generateApp = async ({ serverUrl, name, url, redirectUrl, fields }) => {
+exports.generateApp = async ({ serverUrl, name, url, redirectUrl }) => {
   let returnObj = {
     app: null,
     errors: null,
   };
+
+  const fields = "displayName,firstName,lastName,email";
 
   try {
     const response = await axios.post(serverUrl + "/api/apps/generateApp", {
